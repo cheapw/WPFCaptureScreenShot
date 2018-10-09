452402d51288e82af6c515fd5dcbc178d4370f65
小林 尹 <cheapw@outlook.com>
2018-10-09 11:23:44 +0800

    添加项目文件。

---
+++ WPFCaptureScreenShot/MainWindow.xaml.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+
+namespace WPFCaptureScreenShot
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+        private int currentImageIndex = 0;
+        private int currentColorIndex = 1;
+        private bool isImageIndex_InitialState = true;
+        private bool isColorIndex_InitialState = true;
+        #region 主窗口移动的UI
+        private double x;  // 横坐标
+        private double y;  // 纵坐标
+        private bool isMouseDown = false; // 判断鼠标是否按下
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e) // 鼠标按下的事件处理程序
+        {
+            this.Cursor = Cursors.SizeAll;
+            isMouseDown = true; // 鼠标按下事件发生时将此字段设为true
+            x = e.GetPosition(null).X;  // 获取鼠标当前位置的横坐标
+            y = e.GetPosition(null).Y;  // 获取鼠标当前位置的纵坐标
+        }
+
+        private void Window_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)  // 鼠标移动的事件处理程序
+        {
+            if (isMouseDown)
+            {
+                if (e.GetPosition(null).X > x)
+                {
+                    this.Left += e.GetPosition(null).X - x;
+                }
+                else
+                {
+                    this.Left  -= x-e.GetPosition(null).X;
+                }
+                if (e.GetPosition(null).Y > y)
+                {
+                    this.Top += e.GetPosition(null).Y - y;
+                }
+                else
+                {
+                    this.Top -= y - e.GetPosition(null).Y;
+                }
+            }
+        }
+        private void Window_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            isMouseDown = false;
+            this.Cursor = null;
+        }
+
+
+        #endregion
+
+        private void BtnFullScreenShot_Click(object sender, RoutedEventArgs e)
+        {
+            ScreenShot screenShot = new ScreenShot("");
+            this.Hide();
+            screenShot.Show();
+        }
+
+        private void BtnRectShot_Click_1(object sender, RoutedEventArgs e)
+        {
+            ScreenShot screenShot = new ScreenShot();
+            this.Visibility = Visibility.Collapsed;
+            screenShot.Show();
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            BitmapImage bi = new BitmapImage();
+            bi.BeginInit();
+            bi.UriSource = new Uri($"pack://application:,,,/{button.Tag.ToString()}" + ".jpg", UriKind.Absolute);
+            bi.EndInit();
+            ImageBrush imageBrush = new ImageBrush();
+            imageBrush.ImageSource = bi;
+            imageBrush.Stretch = Stretch.UniformToFill;
+            this.Background = imageBrush;
+        }
+
+        private void btnRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            if (FindResource("DefaultBackground") != null)
+            {
+                this.Background = (ImageBrush)FindResource("DefaultBackground");
+                isColorIndex_InitialState = true;
+                isImageIndex_InitialState = true;
+            }
+        }
+
+        private void BtnColorChange_Click(object sender, RoutedEventArgs e)
+        {
+            if ((string)((sender as Button).Tag) == "bottom")
+            {
+                if (isColorIndex_InitialState)
+                {
+                    this.Background = (LinearGradientBrush)FindResource("1");
+                    isColorIndex_InitialState = false;
+                }
+                else if (currentColorIndex==10)
+                {
+                    currentColorIndex = 1;
+                    this.Background = (LinearGradientBrush)FindResource(currentColorIndex.ToString());
+                }
+                else
+                {
+                    currentColorIndex++;
+                    this.Background = (LinearGradientBrush)FindResource(currentColorIndex.ToString());
+                }
+            }
+            else
+            {
+                if (isColorIndex_InitialState)
+                {
+                    this.Background = (LinearGradientBrush)FindResource("1");
+                    isColorIndex_InitialState = false;
+                }
+                else if (currentColorIndex==1)
+                {
+                    currentColorIndex = 10;
+                    this.Background = (LinearGradientBrush)FindResource(currentColorIndex.ToString());
+                }
+                else
+                {
+                    currentColorIndex--;
+                    this.Background = (LinearGradientBrush)FindResource(currentColorIndex.ToString());
+                }
+            }
+        }
+        private void BtnImageChange_Click(object sender, RoutedEventArgs e)
+        {
+            if ((string)((sender as Button).Tag) == "right")
+            {
+                if (isImageIndex_InitialState)
+                {
+
+                    ChangeBackground(0);
+                    isImageIndex_InitialState = false;
+                }
+                else if (currentImageIndex == 22)
+                {
+                    currentImageIndex = 0;
+                    ChangeBackground(0);
+                }
+                else
+                {
+                    currentImageIndex++;
+                    ChangeBackground(currentImageIndex);
+                }
+            }
+            else
+            {
+                if (isImageIndex_InitialState)
+                {
+                    ChangeBackground(0);
+                    isImageIndex_InitialState = false;
+                }
+                else if (currentImageIndex == 0)
+                {
+                    currentImageIndex = 22;
+                    ChangeBackground(currentImageIndex);
+                }
+                else
+                {
+                    currentImageIndex--;
+                    ChangeBackground(currentImageIndex);
+                }
+            }
+        }
+        private void ChangeBackground(int imageIndex)
+        {
+            BitmapImage bi = new BitmapImage();
+            bi.BeginInit();
+            bi.UriSource = new Uri($"pack://application:,,,/Images/BackImages/{imageIndex}" + ".jpg", UriKind.Absolute);
+            bi.EndInit();
+            ImageBrush imageBrush = new ImageBrush();
+            imageBrush.ImageSource = bi;
+            imageBrush.Stretch = Stretch.UniformToFill;
+            this.Background = imageBrush;
+        }
+
+        
+    }
+}
---
+++ WPFCaptureScreenShot/ScreenShot.xaml.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Drawing;
+using System.Drawing.Imaging;
+using Microsoft.Win32;
+using System.Windows.Threading;
+
+namespace WPFCaptureScreenShot
+{
+    /// <summary>
+    /// ScreenShot.xaml 的交互逻辑
+    /// </summary>
+    public partial class ScreenShot : Window
+    {
+        private double x;  // 横坐标
+        private double y;  // 纵坐标
+        private bool isMouseDown = false; // 判断鼠标是否按下
+        public ScreenShot()
+        {
+            InitializeComponent();
+            this.Loaded -= Window_Loaded;
+        }
+
+        public ScreenShot(string str)
+        {
+            InitializeComponent();
+            this.MouseDown -= Window_MouseDown;
+            this.MouseMove -= Window_MouseMove;
+        }
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            isMouseDown = true; // 鼠标按下事件发生时将此字段设为true
+            x = e.GetPosition(null).X;  // 获取鼠标当前位置的横坐标
+            y = e.GetPosition(null).Y;  // 获取鼠标当前位置的纵坐标
+        }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isMouseDown)  // 如果鼠标已经被按下
+            {
+                // 1. 通过一个矩形来表示目前截图区域
+                System.Windows.Shapes.Rectangle rect = new System.Windows.Shapes.Rectangle();
+                double dx = e.GetPosition(null).X;
+                double dy = e.GetPosition(null).Y;
+                double width = Math.Abs(dx - x);
+                double height = Math.Abs(dy - y);
+                SolidColorBrush brush = new SolidColorBrush(Colors.White);
+                rect.Width = width;
+                rect.Height = height;
+                rect.Fill = brush;
+                rect.Stroke = brush;
+                rect.StrokeThickness = 1;
+                // 通过鼠标按下时与鼠标当前的位置关系来设定矩形与画布左边和顶部的距离
+                if (dx < x)
+                {
+                    Canvas.SetLeft(rect, dx);
+                    if (dy < y)
+                    {
+                        Canvas.SetTop(rect, dy);
+                    }
+                    else
+                    {
+                        Canvas.SetTop(rect, y);
+                    }
+                }
+                else
+                {
+                    Canvas.SetLeft(rect, x);
+                    if (dy < y)
+                    {
+                        Canvas.SetTop(rect, dy);
+                    }
+                    else
+                    {
+                        Canvas.SetTop(rect, y);
+                    }
+                }
+                CaptureCanvas.Children.Clear();
+                CaptureCanvas.Children.Add(rect);
+
+                if (e.LeftButton == MouseButtonState.Released)
+                {
+                    //CaptureCanvas.Children.Clear();
+                    rect.Stroke = new SolidColorBrush(Colors.Red);
+                    rect.StrokeThickness =2;
+
+                    // 2. 获取当前截图区域
+                    if (e.GetPosition(null).X > x)
+                    {
+                        if (e.GetPosition(null).Y > y)
+                        {
+                            //CaptureScreen(x, y, width, height);
+                        }
+                        else
+                        {
+                            //CaptureScreen(x, e.GetPosition(null).Y, width, height);
+                            y = e.GetPosition(null).Y;
+                        }
+                    }
+                    else
+                    {
+                        if (e.GetPosition(null).Y > y)
+                        {
+                            //CaptureScreen(e.GetPosition(null).X, y, width, height);
+                            x = e.GetPosition(null).X;
+                        }
+                        else
+                        {
+                            //CaptureScreen(e.GetPosition(null).X, e.GetPosition(null).Y, width, height);
+                            x = e.GetPosition(null).X;
+                            y = e.GetPosition(null).Y;
+                        }
+                    }
+                    try
+                    {
+                        // 由于选择矩形区域截图时实际截取的图像向右下角偏移了大约7个像素，故作此调整
+                        CaptureScreen(x-7, y-7, width, height);
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("所选区域应为一个矩形，长或宽不能为零！");
+                        return;
+                    }
+                    finally
+                    {
+                        isMouseDown = false;
+                        x = 0.0;
+                        y = 0.0;
+                        this.Hide();
+                        Application.Current.MainWindow.Show();
+                        this.Close();
+                    }
+                }
+            }
+        }
+
+        private void CaptureScreen(double x, double y, double width, double height)
+        {
+            int ix = Convert.ToInt32(x);
+            int iy = Convert.ToInt32(y);
+            int iw = Convert.ToInt32(width);
+            int ih = Convert.ToInt32(height);
+
+            System.Drawing.Bitmap bitmap = new Bitmap(iw, ih);
+            using (Graphics graphics = Graphics.FromImage(bitmap))
+            {
+                graphics.CopyFromScreen(ix, iy, 0, 0, new System.Drawing.Size(iw, ih));
+
+                SaveFileDialog dialog = new SaveFileDialog();
+                dialog.Filter = "Png Files|*.png";
+                if (dialog.ShowDialog() == true)
+                {
+                    bitmap.Save(dialog.FileName, ImageFormat.Png);
+                    
+                    MessageBox.Show("图片已保存至"+dialog.FileName);
+                }
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            CaptureScreen(0, 0, SystemParameters.PrimaryScreenWidth, SystemParameters.PrimaryScreenHeight);
+            this.Hide();
+            Application.Current.MainWindow.Show();
+            this.Close();
+        }
+    }
+}
